69dba80887dd2b4563a77b509e9eca9fc4e2fb81
samarjit mahi <41121431+Samarjiit@users.noreply.github.com>
2021-01-22 18:23:04 +0530

    updating my first android app

---
+++ app/src/main/java/com/example/happybirthday/Question.kt
@@ -0,0 +1,24 @@
+package com.example.happybirthday
+
+import android.view.inputmethod.CorrectionInfo
+
+data class Question (
+    val id:Int,
+    val question: String,
+    val image:Int,
+    val optionOne:String,
+    val optionTwo:String,
+
+    val optionThree:String,
+
+    val optionFour:String,
+
+    val correctionAnswer:Int
+
+
+
+    )
+
+
+
+
